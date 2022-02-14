f0c04ec1dec5d619e4a46354b4002975f297d106
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-02-14 01:36:01 +0100

    moved print statements before operations

--- main.cpp
+++ main.cpp
@@ -41,8 +41,8 @@ void sendReq() {
 	msg_send[1] = medium_req;
 	for(int i = 0; i < world_size; i++) {
 		if(i != rank) {
-			MPI_Send(msg_send, MSG_REQ_SIZE, MPI_INT, i, MSG_REQ, MPI_COMM_WORLD);
 			printf("%c[%dm %d : %d wysyła zapytanie do turysty %d o wejscie do medium %d\n", 0x1B, CYAN, rank, my_clock, i, medium_req);
+			MPI_Send(msg_send, MSG_REQ_SIZE, MPI_INT, i, MSG_REQ, MPI_COMM_WORLD);
 		}
 	}
 }
@@ -50,7 +50,7 @@ void sendReq() {
 void *RecvMsg(void *arg) {
 	while(true) {
 		MPI_Recv(msg_recv, MSG_MAX_SIZE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		printf("%c[%dm %d : %d dostał wiadomosc typu: %d od %d -> msg[0]=%d, msg[1]=%d\n", 0x1B, RED, rank, my_clock, status.MPI_TAG, status.MPI_SOURCE, msg_recv[0], msg_recv[1]);
+		printf("%c[%dm %d : %d dostał wiadomosc typu: %d od %d\n", 0x1B, RED, rank, my_clock, status.MPI_TAG, status.MPI_SOURCE);
 		int sender_clock = msg_recv[0];
 		int sender_id = status.MPI_SOURCE;
 		pthread_mutex_lock(&clock_lock);
@@ -62,14 +62,16 @@ void *RecvMsg(void *arg) {
 				int medium_id = msg_recv[1];
 				pthread_mutex_lock(&release_lock);
 				if(medium_id != medium_req) {
+					printf(" %d : %d wysyla pozowlenie do %d (medium != medium)\n", rank, my_clock, sender_id);
 					MPI_Send(msg_recv, MSG_PERMISSION_SIZE, MPI_INT, sender_id, MSG_PERMISSION, MPI_COMM_WORLD);
 				} else {
 					pthread_mutex_lock(&clock_lock);
 					if((sender_clock < req_clock) || (sender_clock == req_clock && sender_id < rank)) {
+						printf(" %d : %d wysyla pozowlenie do %d (medium = medium)\n", rank, my_clock, sender_id);
 						MPI_Send(msg_recv, MSG_PERMISSION_SIZE, MPI_INT, sender_id, MSG_PERMISSION, MPI_COMM_WORLD);
 					} else {
-						delay_buffer.push_back(sender_id);
 						printf("%c[%dm %d : %d zapisal %d do bufora\n", 0x1B, YELLOW, rank, my_clock, sender_id);
+						delay_buffer.push_back(sender_id);
 					}
 					pthread_mutex_unlock(&clock_lock);
 				}
@@ -77,7 +79,7 @@ void *RecvMsg(void *arg) {
 				break;
 			}
 			case MSG_PERMISSION: {
-				printf("%c[%dm %d : %d dostal zgode od turysty %d -> %d / %d\n", 0x1B, PINK, rank, my_clock, sender_id, PROCESS_NUMBER - ack_counter, PROCESS_NUMBER);
+				printf("%c[%dm %d : %d dostal zgode od turysty %d -> %d / %d\n", 0x1B, PINK, rank, my_clock, sender_id, PROCESS_NUMBER - ack_counter, PROCESS_NUMBER - 1);
 				ack_counter--;
 				if(ack_counter == 0) {
 					sleep(SLEEP_BEFORE_ENTER);
@@ -91,11 +93,11 @@ void *RecvMsg(void *arg) {
 
 void sendDelayAck() {
 	for(int i = 0; i < delay_buffer.size(); ++i) {
-		MPI_Send(msg_send, MSG_PERMISSION_SIZE, MPI_INT, delay_buffer[i], MSG_PERMISSION, MPI_COMM_WORLD);
 		printf("%c[%dm %d : %d wysyla opoznione ACK do %d\n", 0x1B, BLUE, rank, my_clock, delay_buffer[i]);
+		MPI_Send(msg_send, MSG_PERMISSION_SIZE, MPI_INT, delay_buffer[i], MSG_PERMISSION, MPI_COMM_WORLD);
 	}
-	delay_buffer.clear();
 	printf("%c[%dm %d : %d czysci bufor\n", 0x1B, BLUE, rank, my_clock);
+	delay_buffer.clear();
 }
 
 int main(int argc, char **argv) {
